98f626a4a7815d460853f6d0e3321d678ffb033f
Alex Thalhammer <alex@angular-software.at>
2022-10-09 22:21:25 +0200

    09_e2et: Mock the Flight Search

--- cypress/integration/flight-booking.spec.ts
+++ cypress/integration/flight-booking.spec.ts
@@ -14,4 +14,13 @@ describe('Flight Search E2E Test', () => {
     cy.get('form .btn').click();
     cy.get('flight-card').its('length').should('be.gte', 3);
   });
+
+  it('should search for flights from Wien to Eisenstadt by intercepting the network', () => {
+    cy.visit('/flight-booking/flight-search');
+    cy.fixture('flights').then((flights) => cy.intercept('GET', 'http://www.angular.at/api/flight**', flights));
+    cy.get('input[name=from]').clear().type('Wien');
+    cy.get('input[name=to]').clear().type('Eisenstadt');
+    cy.get('form .btn').click();
+    cy.get('flight-card').should('have.length', 3);
+  });
 });
