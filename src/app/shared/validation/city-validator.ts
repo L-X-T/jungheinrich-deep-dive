L-X-T/mf-deep-dive

07b_rf: Parametrizable Validators

src/app/shared/validation/city-validator.ts
@@ -1,15 +1,16 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export const validateCity = (c: AbstractControl): ValidationErrors | null => {
-  const validCities: string[] = ['Graz', 'Wien', 'Hamburg', 'Berlin'];
-  if (c.value && validCities.indexOf(c.value) === -1) {
-    return {
-      city: {
-        actualValue: c.value,
-        validCities
-      }
-    };
-  }
+export const validateCity =
+  (validCities: string[]): ValidatorFn =>
+  (c: AbstractControl): ValidationErrors | null => {
+    if (c.value && validCities.indexOf(c.value) === -1) {
+      return {
+        city: {
+          actualValue: c.value,
+          validCities
+        }
+      };
+    }
 
-  return null;
-};
+    return null;
+  };

L-X-T/jungheinrich-deep-dive

src/app/shared/validation/city-validator.ts
@@ -1,15 +1,16 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export const validateCity = (c: AbstractControl): ValidationErrors | null => {
-  const validCities: string[] = ['Graz', 'Wien', 'Hamburg', 'Berlin'];
-  if (c.value && validCities.indexOf(c.value) === -1) {
-    return {
-      city: {
-        actualCity: c.value,
-        validCities
-      }
-    };
-  }
+export const validateCity =
+  (validCities: string[]): ValidatorFn =>
+  (c: AbstractControl): ValidationErrors | null => {
+    if (c.value && validCities.indexOf(c.value) === -1) {
+      return {
+        city: {
+          actualCity: c.value,
+          validCities
+        }
+      };
+    }
 
-  return null;
-};
+    return null;
+  };